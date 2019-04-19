274b1ea94df53357e58731b29fd9710c4a86ebdf
hmeaton <42755102+hmeaton@users.noreply.github.com>
2019-04-19 13:55:56 -0500

    add customer

--- crm/views.py
+++ crm/views.py
@@ -40,6 +40,23 @@ def customer_edit(request, pk):
 
 
 @login_required
+def customer_new(request):
+    if request.method == "POST":
+        form = CustomerForm(request.POST)
+        if form.is_valid():
+            customer = form.save(commit=False)
+            customer.created_date = timezone.now()
+            customer.save()
+            customer = Customer.objects.filter(created_date__lte=timezone.now())
+            return render(request, 'crm/customer_list.html',
+                          {'customers': customer})
+    else:
+        form = CustomerForm()
+        # print("Else")
+    return render(request, 'crm/customer_new.html', {'form': form})
+
+
+@login_required
 def customer_delete(request, pk):
     customer = get_object_or_404(Customer, pk=pk)
     customer.delete()
